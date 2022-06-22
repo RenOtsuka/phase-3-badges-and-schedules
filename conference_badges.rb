940451e0184b90a5c36ac5f031b43617ac7b4a46
Ryan Delos Santos <ryansantos.delossantos121@gmail.com>
2022-06-22 12:06:09 -0500

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,29 @@
 # Write your code here.
+
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+    array.map { |name|
+        badge_maker(name)
+    }
+end
+
+def assign_rooms(array)
+    result = []
+    array.each_with_index do |name, index|
+        result << "Hello, #{name}! You'll be assigned to room #{index + 1}!"
+    end
+    result
+end
+
+def printer(array)
+    batch_badge_creator(array).map { |arg|
+        puts arg
+    }
+
+    assign_rooms(array).map { |arg|
+        puts arg
+    }
+end
